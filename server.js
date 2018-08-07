7458196dbe3bced3bbe3c04646121f725209b284
ByoungSul lee <byoungsullee@ByoungSului-MacBook-Pro.local>
2018-08-07 13:34:22 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+
+hbs.registerPartials(__dirname+'/views/partials');
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt',(txt)=>{
+    return txt.toUpperCase(); 
+});
+app.set('view engine','hbs');
+app.use((req,res,next)=>{
+    var now = new Date().toString();
+    var log = `${now} ${req.method} ${req.url}`;
+    console .log(log);
+    fs.appendFile('server.log',log+ '\n',(err)=>{
+        if(err){
+            console.log('unable to append to server.log');
+        }
+    });
+    
+    next();
+});//NEEDS to call next (LOGGER)
+
+/*app.use((req,res,next)=>{
+    res.render('maintenance.hbs');
+});*/
+
+app.use(express.static(__dirname + `/public`));// Middleware for static directory
+
+app.get('/',(req,res)=>{
+    //res.send('<h1>Hello express</h1>');
+    res.render('home.hbs',{
+        pageTitle:'Home Page',
+        welcomeMsg:'Welcome to the Website',
+        //currentYear:new Date().getFullYear() 
+    });
+    /*res.send({
+        name:'Lee',
+        likes:[
+            'comp',
+            'people'
+        ]
+    });*/
+});
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        pageTitle:'About Page',
+        //currentYear:new Date().getFullYear() 
+    }); 
+});
+
+
+app.get('/bad',(req,res)=>{
+    res.send({
+        msg:'BAD ROUTE'
+    });
+});
+app.listen(3000,()=>{
+    console.log('Sever is up');
+});       
